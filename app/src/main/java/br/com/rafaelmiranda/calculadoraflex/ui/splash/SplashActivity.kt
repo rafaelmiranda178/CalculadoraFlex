3e0eb463e962cbf4e27202711fce303017d765c3
Logon Aluno <logonaluno@local.com>
2019-08-17 11:01:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/rafaelmiranda/calculadoraflex/ui/splash/SplashActivity.kt
@@ -0,0 +1,62 @@
+package br.com.rafaelmiranda.calculadoraflex.ui.splash
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import br.com.rafaelmiranda.calculadoraflex.R
+import br.com.rafaelmiranda.calculadoraflex.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val TEMPO_AGUARDO_SPLASHSCREEN = 3500L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val preferences =
+            getSharedPreferences("user_preferences",
+                Context.MODE_PRIVATE)
+        val isFirstOpen = preferences.getBoolean("open_first", true)
+        if(isFirstOpen){
+            markAppAlreadyOpen(preferences)
+            showSplash()
+        }else{
+            showLogin()
+        }
+    }
+
+    private fun markAppAlreadyOpen(preferences : SharedPreferences){
+        val editor = preferences.edit()
+        editor.putBoolean("open_first", false)
+        editor.apply()
+    }
+
+    private fun showLogin(){
+        val nextScreen = Intent(this@SplashActivity, LoginActivity::class.java)
+        startActivity(nextScreen)
+        finish()
+    }
+
+    private fun showSplash(){
+        //Carrega a animação
+        val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
+        anim.reset()
+        ivLogo.clearAnimation()
+        //Roda a animação
+        ivLogo.startAnimation(anim)
+        //Chama a próxima tela após 3,5 segundos definido na SPLASH_DISLAY_LENGTH
+        Handler().postDelayed({
+            val proximaTela = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(proximaTela)
+            finish()
+        }, TEMPO_AGUARDO_SPLASHSCREEN)
+
+    }
+
+
+}
